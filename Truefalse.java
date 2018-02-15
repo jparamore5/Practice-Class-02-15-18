efed163ffc545cfb610c9d33278977f681c3ef72
Jonathan Mark Paramore <jonathanmarkparamore@Jonathans-MacBook-Air.local>
2018-02-15 16:30:01 -0500

    first commit

---
+++ Truefalse.java
@@ -0,0 +1,25 @@
+package oddNumber;
+
+public class Truefalse {
+	
+	public static void main(String[]args) {
+		System.out.println("The numer is even or odd ?" + oddNumber(2));
+		
+			}
+
+	public static boolean oddNumber(int num1) {
+		if (num1 %2 == 0) {
+			return true;
+		} else {
+			return false;}
+		
+		
+	}
+		
+		public static int multiplyNumbers(int num1, int num2) {
+
+			int multiply = num1 * num2;
+			return multiply;
+
+}
+}
